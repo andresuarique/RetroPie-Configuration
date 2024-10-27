8576c9920163c5adb00fe4c4ce3580d9c0a9777c
Andrés Suarique <42348158+andresuarique@users.noreply.github.com>
2024-10-27 17:59:09 -0500

    Update install_themes.sh

--- scripts/install_themes.sh
+++ scripts/install_themes.sh
@@ -1,6 +1,8 @@
 cd .. || exit 1
 cd .. || exit 1
-cd etc/emulationstation/themes/
+cd etc || exit 1
+cd emulationstation || exit 1
+cd themes || exit 1
 git clone --depth=1 https://github.com/c64-dev/es-theme-epicnoir epic-noir
 git clone --depth=1 https://github.com/lipebello/es-theme-retrorama retrorama
 git clone --depth=1 https://github.com/lioneil/obsidian-es-theme-retropie obsidian
