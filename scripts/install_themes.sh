77d27dc085da0fb68d2f086afb18525a8d27c970
Andrés Suarique <42348158+andresuarique@users.noreply.github.com>
2024-10-27 17:41:41 -0500

    Add files via upload

---
+++ scripts/install_themes.sh
@@ -0,0 +1,8 @@
+cd .. || exit 1
+cd .. || exit 1
+cd etc/emulationstation/themes
+git clone --depth=1 https://github.com/c64-dev/es-theme-epicnoir epic-noir
+git clone --depth=1 https://github.com/lipebello/es-theme-retrorama retrorama
+git clone --depth=1 https://github.com/lioneil/obsidian-es-theme-retropie retrorama
+git clone --depth=1 https://github.com/fabricecaruso/batocera-emulationstation.git nostalgia-pure
+git clone --depth=1 https://github.com/fagnerpc/Alekfull-ARTFLIX.git artflix
