d4e50cba177ed81b31020404cda988635bc4e7b2
Andres Suarique <andresuarique@gmail.com>
2024-10-27 19:06:33 -0500

    Update install_themes.sh

--- scripts/install_themes.sh
+++ scripts/install_themes.sh
@@ -8,3 +8,4 @@ git clone --depth=1 https://github.com/c64-dev/es-theme-epicnoir epic-noir
 git clone --depth=1 https://github.com/lipebello/es-theme-retrorama retrorama
 git clone --depth=1 https://github.com/lioneil/obsidian-es-theme-retropie obsidian
 git clone --depth=1 https://github.com/fabricecaruso/batocera-emulationstation.git nostalgia-pure
+git clone --depth=1 https://github.com/fagnerpc/Alekfull-ARTFLIX.git artflix
