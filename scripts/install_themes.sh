4bd93dde15463bf20bdb76baf4e08887c307281f
Andres Suarique <andresuarique@gmail.com>
2024-10-27 19:09:42 -0500

    Update install_themes.sh

--- scripts/install_themes.sh
+++ scripts/install_themes.sh
@@ -4,8 +4,8 @@ cd .. || exit 1
 cd etc/ || exit 1
 cd emulationstation || exit 1
 cd themes || exit 1
-git clone --depth=1 https://github.com/c64-dev/es-theme-epicnoir epic-noir
-git clone --depth=1 https://github.com/lipebello/es-theme-retrorama retrorama
-git clone --depth=1 https://github.com/lioneil/obsidian-es-theme-retropie obsidian
-git clone --depth=1 https://github.com/fabricecaruso/batocera-emulationstation.git nostalgia-pure
-git clone --depth=1 https://github.com/fagnerpc/Alekfull-ARTFLIX.git artflix
+sudo git clone --depth=1 https://github.com/c64-dev/es-theme-epicnoir epic-noir
+sudo git clone --depth=1 https://github.com/lipebello/es-theme-retrorama retrorama
+sudo git clone --depth=1 https://github.com/lioneil/obsidian-es-theme-retropie obsidian
+sudo git clone --depth=1 https://github.com/fabricecaruso/batocera-emulationstation.git nostalgia-pure
+sudo git clone --depth=1 https://github.com/fagnerpc/Alekfull-ARTFLIX.git artflix
