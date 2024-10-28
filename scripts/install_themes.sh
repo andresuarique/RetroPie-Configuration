244dbe6f46c5d91059c9b0ff1958b30bbb765ed4
Andres Suarique <andresuarique@gmail.com>
2024-10-27 19:08:08 -0500

    Update install_themes.sh

--- scripts/install_themes.sh
+++ scripts/install_themes.sh
@@ -2,7 +2,7 @@ cd ~ || exit 1
 cd .. || exit 1
 cd .. || exit 1
 cd etc/ || exit 1
-cd emulatiostation || exit 1
+cd emulationstation || exit 1
 cd themes || exit 1
 git clone --depth=1 https://github.com/c64-dev/es-theme-epicnoir epic-noir
 git clone --depth=1 https://github.com/lipebello/es-theme-retrorama retrorama
